ec1e6c2ec30ffb8ddf8eb275ab9971d50ddb27f2
deyanslavov <pikacho2013@gmail.com>
2018-07-25 23:38:15 +0300

    test

---
+++ Cashy/Cashy.App/DataModels/ApplicationPage.cs
@@ -0,0 +1,14 @@
+﻿namespace Cashy.App.DataModels
+{
+    /// <summary>
+    /// A page of the application
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// The initial login page
+        /// </summary>
+        Login,
+
+    }
+}
