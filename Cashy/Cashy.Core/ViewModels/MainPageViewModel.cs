3b4d04b7101df97f9adfda250b5ee78f910864f2
deyanslavov <deyanslavov41@gmail.com>
2018-08-09 22:46:07 +0300

    LeftSideMenuButton added

--- Cashy/Cashy.Core/ViewModels/MainPageViewModel.cs
+++ Cashy/Cashy.Core/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,13 @@
 ﻿namespace Cashy.Core
 {
+    using System;
+    using System.Globalization;
+
+    /// <summary>
+    /// Thew View Model for the main screen
+    /// </summary>
     public class MainPageViewModel : BaseViewModel
     {
+        public string DateTimeNow => DateTime.Now.ToString("", CultureInfo.InvariantCulture);
     }
 }
