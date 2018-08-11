8c8fe46ccc6eca40c907d5c30cd8893b204de81b
deyanslavov <deyanslavov41@gmail.com>
2018-08-11 17:40:04 +0300

    RightSideMenu control created including buttons

---
+++ Cashy/Cashy.App/Controls/SideMenus/RightSideMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Cashy.App
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for LeftSideMenu.xaml
+    /// </summary>
+    public partial class RightSideMenu : UserControl
+    {
+        public RightSideMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
