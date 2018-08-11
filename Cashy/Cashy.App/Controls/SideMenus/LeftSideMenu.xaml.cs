73c4c7cf18d77d92e4f777e314878559fff2b9a4
deyanslavov <deyanslavov41@gmail.com>
2018-08-11 17:28:26 +0300

    LeftSideMenu control created including buttons

---
+++ Cashy/Cashy.App/Controls/SideMenus/LeftSideMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Cashy.App
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for LeftSideMenu.xaml
+    /// </summary>
+    public partial class LeftSideMenu : UserControl
+    {
+        public LeftSideMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Cashy/Cashy.Core/ViewModels/Main/MainPageViewModel.cs
+++ Cashy/Cashy.Core/ViewModels/Main/MainPageViewModel.cs
@@ -1,14 +1,16 @@
 ﻿namespace Cashy.Core
 {
     using System;
-    using System.Collections.Generic;
-    using System.Globalization;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
 
     /// <summary>
     /// Thew View Model for the main screen
     /// </summary>
     public class MainPageViewModel : BaseViewModel
     {
+        #region Public Properties
+
         /// <summary>
         /// The current date time to be displayed at the top of the page
         /// </summary>
@@ -23,5 +25,45 @@
         /// The currency the user selected
         /// </summary>
         public string Currency { get; set; }
+        
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command to show the left side menu
+        /// </summary>
+        public ICommand ShowLeftSideMenuCommand { get; set; }
+
+        /// <summary>
+        /// The command to show the right side menu
+        /// </summary>
+        public ICommand ShowRightSideMenuCommand { get; set; }
+
+        #endregion
+
+        #region Construtor
+
+        public MainPageViewModel()
+        {
+            this.ShowLeftSideMenuCommand = new RelayCommand(async () => await ShowLeftSideMenu());
+            this.ShowRightSideMenuCommand = new RelayCommand(async () => await ShowRightSideMenu());
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        private Task ShowRightSideMenu()
+        {
+            throw new NotFiniteNumberException();
+        }
+
+        private Task ShowLeftSideMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
